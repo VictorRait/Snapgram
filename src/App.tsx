b6a364a93f79731bcb21312cbe4ab26533db5279
VictorRait <victorlemuelrait@gmail.com>
2023-12-23 01:20:20 +0800

    add a bunch of starter pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,13 @@ function App() {
 				<Route element={<RootLayout />}>
 					<Route index element={<Home />} />
 					<Route path="/explore" element={<Explore />} />
-					<Route element={<Home />} />
-					<Route element={<Home />} />
+					<Route path="/saved" element={<Saved />} />
+					<Route path="/all-users" element={<AllUsers />} />
+					<Route path="/create-post" element={<CreatePost />} />
+					<Route path="/update-post/:id" element={<EditPost />} />
+					<Route path="/posts/:id" element={<PostDetails />} />
+					<Route path="/profile/:id/*" element={<Profile />} />
+					<Route path="/update-profile/:id" element={<UpdateProfile />} />
 				</Route>
 			</Routes>
 			<Toaster />
